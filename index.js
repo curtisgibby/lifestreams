1dbc0383ca1867e3dc2f6456e9d7d38d76b22365
Curtis Gibby <cgibby@gmail.com>
2022-01-03 12:19:37 -0700

    add tooltip with data
    fix #7

--- index.js
+++ index.js
@@ -37,15 +37,54 @@ const xAxis = d3.axisBottom()
 svg.append("g")
     .call(xAxis);
 
+const tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .style("padding", ".1rem")
+    .style("background", "#dddddddd");
+
 for(const stream of streams) {
     const y = stream.index * (streamHeight + streamMargin);
     for(const event of stream.events) {
+        let tooltipHtml = '';
+        if (stream.label) {
+            tooltipHtml += '<h4>' + stream.label + '</h4>';
+        }
+        if (event.label) {
+            tooltipHtml += '<h5>' + event.label + '</h5>';
+        }
+        if (event.startDate || event.endDate) {
+            tooltipHtml += '<p><i>';
+            if (event.startDate) {
+                tooltipHtml += new Date(event.startDate).toLocaleDateString();
+            }
+            tooltipHtml += '–';
+            if (event.endDate) {
+                tooltipHtml += new Date(event.endDate).toLocaleDateString();
+            }
+            tooltipHtml += '</i></p>';
+        }
+        if (event.imageUrl) {
+            tooltipHtml += '<img style="max-width: 200px; max-height: 200px;" src="' + event.imageUrl + '">';
+        }
         svg.append('line')
             .style("stroke", event.color || stream.color)
             .style("stroke-width", streamHeight)
             .attr("x1", timeScale(new Date(event.parsedStartDate)))
             .attr("y1", y)
             .attr("x2", timeScale(new Date(event.parsedEndDate)))
-            .attr("y2", y);
-    }
+            .attr("y2", y)
+            .attr("data-html", tooltipHtml)
+            .on("mouseover", function(d) {
+                tooltip.html(d.target.getAttribute('data-html'))
+                    .style("visibility", "visible")
+                    .style("left", (d.clientX) + "px")
+                    .style("top", (d.clientY - 28) + "px");
+                })
+            .on("mouseout", function(d) {
+                tooltip.style("visibility", "hidden");
+            });
+        }
 }
